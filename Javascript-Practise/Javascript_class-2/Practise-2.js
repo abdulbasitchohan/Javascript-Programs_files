8862ff2657e47fd3191f0e33a9ebb59fa9469160
ABC TECH <abdulbasitchohan40@gmail.com>
2025-10-30 17:52:53 -1200

    Add files via upload

---
+++ Javascript-Practise/Javascript_class-2/Practise-2.js
@@ -0,0 +1,57 @@
+
+// const name = prompt("what's Your Name");
+// const age = prompt("what's Your age");
+
+// alert("Hello" + " " + name + " " + "Your are" + " " + age + " " + "year Old.");
+
+
+// THIS IS "VAR" VARIABLE 
+
+
+// var Name = "AbdulBasit";
+// var Age = "17";
+// var Course = "Modern Web Development";
+// var Branch = "Autobhan-Campus";
+// var City = "Hyderabad";
+
+// console.log(Name + " " + Age +  " " + Course +  " " + Branch +  " " + City);
+
+
+
+// THIS IS CONST VARIABLE
+
+
+// const Name = "Abdul Basit";
+// const Age = "17";
+// const Course = "Modern Web Development";
+// const Branch = "Autobhan-Campus";
+// const City = "Hyderabad";
+
+// console.log(Name + "" + Age + "" + Course + "" + Branch + "" + City);
+
+
+// THIS IS "LET" VARIABLE 
+
+
+// let Name = "Abdul-Basit";
+// let Age = "17";
+// let Course = "Modern Web Development";
+// let Branch = "Autobhan-Campus";
+// let City = "Hyderabad";
+
+// console.log(Name + " " + Age + " " + Course + " " + City + " " + Branch  );
+
+
+// THIS IS QUESTION POP UP 
+
+
+const Q_1 = prompt("What's Your Name?");
+const Q_2 = prompt("What's Your Age?");
+
+alert("Listen To me Bro")
+
+alert("Hello" + " " + Q_1 + " " + "Your age" + " " + Q_2 + " " + "Year old.");
+
+alert("I'm right Bro.");
+
+alert("I'm Lengend.");
