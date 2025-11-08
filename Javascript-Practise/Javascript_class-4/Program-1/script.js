f1e386922eacd70816791b1b17aa16134a3b11ea
abdulbasitchohan <abdulbasitchohan40@gmail.com>
2025-11-08 18:54:49 +0500

    Class-4 of program-1 add files

---
+++ Javascript-Practise/Javascript_class-4/Program-1/script.js
@@ -0,0 +1,6 @@
+let FirstNum = prompt("Enter Any Choose Number.");
+let SecondNum = prompt("Enter Than Any Choose Number.");
+
+let Operator = prompt("Any Sign Use : + , - , x , ÷ ");
+
+let Result = alert(FirstNum, "+" , SecondNum);
