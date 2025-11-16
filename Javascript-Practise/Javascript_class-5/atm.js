b7ed5578a90ba45915f9cf747231b1ee4439a333
abdulbasitchohan <abdulbasitchohan40@gmail.com>
2025-11-16 17:51:49 +0500

    js ATM Changes

--- Javascript-Practise/Javascript_class-5/atm.js
+++ Javascript-Practise/Javascript_class-5/atm.js
@@ -1,43 +1,51 @@
-let balance = 1000;
 
-const SavedPin = 123;
+// This is Balance for user 
+let balance = 2000;
 
-let withdrawAmount; //withdrawAmount variable
+const SavedPin = 123
 
+alert("Welcome To JS ATM");
 let UserPin = Number(prompt("Enter Your PIN Code."));
 
 if (SavedPin === UserPin) {
-    let choice = prompt(
-        "Welcome to JS ATM 💳\n\n" +
-        "1️⃣ Check Balance\n" +
-        "2️⃣ Withdraw\n" +
-        "3️⃣ Deposit\n" +
-        "4️⃣ Exit\n\n" +
-        "Enter your choice (1-4):"
-    );
+     let choice = prompt(
+         "Welcome to JS ATM 💳\n\n" +
+         "1️⃣ Check Balance\n" +
+         "2️⃣ Withdraw\n" +
+         "3️⃣ Deposit\n" +
+         "4️⃣ Exit\n\n" +
+         "Enter your choice (1-4):"
+     );
 
-    if (choice === "1") {
-        alert("Your balance is: $" + balance);
+  if (choice === "1") {
+    alert("Your Balance is: $" + balance);
+  }
+
+ else if (choice === "2") {
+    let withdrawAmount; //withdrawAmount Variable
+    withdrawAmount = prompt("Enter your Withdraw Amount!");
+
+    if (withdrawAmount > balance) {
+        alert("Insufficient Funds!")
+        
+    }else{
+        balance -= withdrawAmount;
+        alert("Withdraw Successful !");
+        alert("Your New Balance: $" + balance);
     }
+   
+}else{
 
-    else if (choice === "2") {
-        withdrawAmount = prompt("Enter your Withdraw Amount!");
-
-        if (withdrawAmount > balance) {
-            alert("Insufficient funds!");
-        }else{   
-            balance -= withdrawAmount;
-            alert("Withdrawal successful! New balance: $" + balance);
-        }
-
-    }
-
-    else {
-    }
 }
 
-else {
-    alert("Incorrect PIN!");
+if (choice === "4") {
+    alert("Thanks For Use JS ATM.");
+ }else{}
 
+
+
+}else{
+    alert("Incorrect PIN Code!");
 }
+
 
