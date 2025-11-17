0bb1e1956f462e4f73a45f97f14c181c67dbaf74
abdulbasitchohan <abdulbasitchohan40@gmail.com>
2025-11-17 14:42:16 +0500

    JS class 6 add

---
+++ Javascript-Practise/Javascript_class-6/Practise-1.js
@@ -0,0 +1,38 @@
+// Print numbers from 1 to 20.
+
+// for (let count = 1; count <= 20; count++) { 
+    // console.log(count);
+// }
+
+
+// Print even numbers from 2 to 20.
+
+
+// let even = 1;
+// for (even; even <= 10; even++) {
+//     console.log("2 x "  + even + " = " + 2 * even);
+    
+// }
+
+
+// Print numbers from 10 to 1 (reverse).
+
+// for (let count = 10; count >= 0; count--) { 
+//     console.log(count);
+// }
+
+
+// Print the multiplication table of 7.
+
+// let num = 1
+// for (num; num <= 10; num++) {
+//     console.log("7 x " + num + " = " + 7 * num);
+       
+// }
+
+
+let num = 0;
+for (num; num <= 50; num++) {
+    console.log(num + num);
+    
+}
