f42d68b402d71613d65d6973d9b28ce7243a80f6
abdulbasitchohan <abdulbasitchohan40@gmail.com>
2025-11-20 15:06:56 +0500

    Atm task add 4 choisce option and full complete.

--- Javascript-Practise/Javascript_class-5/atm.js
+++ Javascript-Practise/Javascript_class-5/atm.js
@@ -4,55 +4,56 @@ let balance = 2000;
 
 const SavedPin = 123
 
-alert("Welcome To JS ATM");
+alert("Welcome To JS ATM.➡");
 let UserPin = Number(prompt("Enter Your PIN Code."));
 
 if (SavedPin === UserPin) {
-     let choice = prompt(
-         "Welcome to JS ATM 💳\n\n" +
-         "1️⃣ Check Balance\n" +
-         "2️⃣ Withdraw\n" +
-         "3️⃣ Deposit\n" +
-         "4️⃣ Exit\n\n" +
-         "Enter your choice (1-4):"
-     );
+    let choice = prompt(
+        "Welcome to JS ATM 💳\n\n" +
+        "1️⃣ Check Balance\n" +
+        "2️⃣ Withdraw\n" +
+        "3️⃣ Deposit\n" +
+        "4️⃣ Exit\n\n" +
+        "Enter your choice (1-4):"
+    );
 
-  if (choice === "1") {
-    alert("Your Balance is: $" + balance);
-  }
-
- else if (choice === "2") {
-    let withdrawAmount; //withdrawAmount Variable
-    withdrawAmount = prompt("Enter your Withdraw Amount!");
-
-    if (withdrawAmount > balance) {
-        alert("Insufficient Funds!")
-        
-    }else{
-        balance -= withdrawAmount;
-        alert("Withdraw Successful !");
-        alert("Your New Balance: $" + balance);
+    if (choice === "1") {
+        alert("Your Balance is: $" + balance);
     }
-   
-}else{
 
-}
+    else if (choice === "2") {
+        let withdrawAmount; //withdrawAmount Variable
+        withdrawAmount = prompt("Enter your Withdraw Amount!");
 
-if (choice === "2") {
-    let deposit;
-    deposit = prompt("Enter Your Deposit Amount!");
-} else {
-    
-}
+        if (withdrawAmount > balance) {
+            alert("Insufficient Funds!")
 
-if (choice === "4") {
-    alert("Thanks For Use JS ATM.");
- }else{}
+        } else {
+            balance -= withdrawAmount;
+            alert("✔ Withdraw Successful 🎉");
+            alert("Your New Balance: $" + balance);
+        }
+    }
+        if (choice === "3") {
+            let DepositAmount; //Deposit Variable
+            DepositAmount = Number(prompt("Enter your Withdraw Amount!"));
+
+            balance += DepositAmount;
+            alert("✅Deposit Process Successful 🎉");
+            alert("Your New Balance: $" + balance);
+            alert("Thanks for Use JS ATM.")
+            } else {
+            }
+
+
+            if (choice === "4") {
+                alert("Thanks For Use JS ATM.");
+            } else { }
 
 
 
-}else{
-    alert("Incorrect PIN Code!");
-}
+        } else {
+            alert("Incorrect PIN Code ❗ ");
+        }
 
 
