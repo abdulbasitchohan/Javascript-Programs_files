e45b8c3db7f1815d04782c6e6c0de498c881ef72
abdulbasitchohan <abdulbasitchohan40@gmail.com>
2025-11-22 20:19:36 +0500

    Task 2 Complete odd number

--- Javascript-Practise/Javascript_class-7/index.js
+++ Javascript-Practise/Javascript_class-7/index.js
@@ -5,3 +5,27 @@
 //     num++;
 // }
 
+
+// Print odd numbers from 1 to 15.
+// METHOD NO : 1
+
+// let odd = 1;
+
+// while (odd <= 15) {
+//     console.log(odd)
+//     odd += 2;
+// }
+
+
+// Print odd numbers from 1 to 15. 
+// METHOD NO : 2
+
+// let odd = 1;
+// while (odd <= 15) {
+//     if (odd % 2 !==0) {
+//         console.log(odd);
+//     }
+//     odd++;
+
+// }
+
