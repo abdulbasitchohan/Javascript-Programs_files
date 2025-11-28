1559b312bca990d0a2bbb632349ba83499bfdf3a
abdulbasitchohan <abdulbasitchohan40@gmail.com>
2025-11-28 10:32:53 +0500

    add task 2

---
+++ Javascript-Practise/Javascript_class-8/index.js
@@ -0,0 +1,24 @@
+// function greet(Name) {
+//     console.log(`Hello ${Name}`);
+    
+// }
+
+// greet(`Basit`); 
+
+
+
+// function sum(a, b) {
+//     // console.log(`${a + b}`);
+//     return a + b;
+// }
+
+// let result = sum(5 , 4);
+
+// console.log(result);
+
+
+// function MaxNum(Num_1, Num_2 , Num_3) {
+//     return Math.max(Num_1, Num_2 , Num_3);
+// }
+// let result = MaxNum(1 , 2 ,3 );
+// console.log(result);
