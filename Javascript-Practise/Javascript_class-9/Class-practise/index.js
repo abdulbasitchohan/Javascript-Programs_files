865471aec989d86bf3e483cbfae3eb6be8ea8af4
abdulbasitchohan <abdulbasitchohan40@gmail.com>
2025-12-03 22:36:27 +0500

    JS Class 9 add for Arrays

---
+++ Javascript-Practise/Javascript_class-9/Class-practise/index.js
@@ -0,0 +1,24 @@
+// let countries = ["Pakistan", "England", "Japan", "China", "Russia"]
+
+// countries.push("Afghanistan");
+// countries.pop();
+
+// console.log(countries);
+
+// let Usertype = prompt("Seach Countries !");
+// let search = countries.includes(Usertype);
+
+// alert(search);
+
+
+
+// let Nums = [ 1,2,3,4,5,6,7,8,9,10];
+let Nums = [];
+
+for (let i = 0; i <= 50; i++) {
+    
+    if (Nums % 2 !== 0) {
+        console.log(Nums);
+    }
+}
+
